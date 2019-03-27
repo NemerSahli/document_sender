4b9e1b1cdec04984d9c942dcfcd0c03991a707d7
Nemer EL-Sahli <nemer.sahli@gmx.de>
2019-03-27 15:49:50 +0100

    Nemer1.0: created new gmail accout by the Mieter Engel to autherized to send mails via npm nodemailer

---
+++ src/server/mailHandler/mailSender.js
@@ -0,0 +1,27 @@
+var nodemailer = require('nodemailer');
+
+function sendMail(recipientAddress, subject, body) {
+  var smtpConfig = {
+   host: 'smtp.gmail.com',
+   port: 465,
+   secure: true,
+    auth: {
+      user: 'mieter.engel24@gmail.com',
+      pass: 'Engel2019'
+    }
+  };
+
+  var transporter = nodemailer.createTransport(smtpConfig);
+
+  var mailOptions = {
+    from: ' "Mieter Engel" <mieter.engel24@gmail.com>',
+    to: recipientAddress,
+    subject: subject,
+    text: 'Hello World',
+    html: body
+  };
+  transporter.sendMail(mailOptions, function(err, info) {
+    if (err) console.log('mail was not delivered');
+  });
+}
+module.exports.sendMail = sendMail;
--- src/server/routes/mail.js
+++ src/server/routes/mail.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const randomstring = require('randomstring');
+const mailSender = require('../mailHandler/mailSender');
 
 router.get('/', (req, res) => {
   res.send({
@@ -12,6 +13,12 @@ router.post('/send', (req, res) => {
   if (!req.body) {
     return res.send({ error: 'data is required' });
   }
+  let mailBody = `<h3>Mieter Engel</h3>
+  <p>Thanks for your contact</p>
+  <p>yours sincerely</p> 
+  <p>lawyer</p>`;
+  mailSender.sendMail('nemer.sahli@gmail.com', 'Contract document', mailBody);
+
   res.send({
     error: 0,
     somedata: 'hallo world ->' + req.body.userName + randomstring.generate(10)
