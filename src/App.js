0c35069aa3f6eb314613e8ce6d786c8bbd9bbb4b
Nemer EL-Sahli <nemer.sahli@gmx.de>
2019-04-09 22:46:23 +0200

    added config.json for ENV

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import DocumentUploader from './components/pages/DocumentUploader';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    window.MieterBackend = config.backend;
+    window.MieterHost = config.host;
+  }
   render() {
     return (
       <div className="App">
